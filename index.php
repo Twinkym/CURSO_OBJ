9d5be1091ce2b809e121f0272df27f0bed961cd8
David De La Puente Enriquez <Twinkymen@hotmail.com>
2024-06-06 18:33:34 +0200

    First steps in laragon & snow animation

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="Es-es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="David De La Puente" >
+    <title>Pruebas PHP</title>
+    <!-- Styles -->
+     <link rel="stylesheet" href="./css/styles.css" type="text/css">
+</head>
+<body>
+    <h2>Página desde el Servidor Laragon</h2>
+    <div id="caja1" class="caja"></div>
+    <div id="caja2" class="caja"></div>
+    <div id="caja3" class="caja"></div>
+    <div id="caja4" class="caja"></div>
+    <div id="caja5" class="caja"></div>
+    <div id="caja6" class="caja"></div>
+    <div id="caja7" class="caja"></div>
+    <div id="caja8" class="caja"></div>
+    <div id="caja9" class="caja"></div>
+    <div id="caja10" class="caja"></div>
+    <div id="caja11" class="caja"></div>
+    <div id="caja12" class="caja"></div>
+
+    <div id="nevar" class="nevar">
+        <div class="copos c1">&#10052<span>*</span></div>
+        <div class="copos c2">&#10057<span>*</span></div>
+        <div class="copos c3">&#10059<span>*</span></div>
+        <div class="copos c4">&#10053<span>*</span></div>
+        <div class="copos c5">&#10057<span>*</span></div>
+        <div class="copos c6">&#10059<span>*</span></div>
+        <div class="copos c7">&#10053<span>*</span></div>
+        <div class="copos c8">&#10052<span>*</span></div>
+    </div>
+
+</body>
+</html>

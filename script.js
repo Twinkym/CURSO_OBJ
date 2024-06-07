6ae5275ea11327c7be2f8c333942b28eacdc1ac6
David De La Puente Enriquez <Twinkymen@hotmail.com>
2024-06-07 17:23:18 +0200

    Generador version con select

---
+++ script.js
@@ -0,0 +1,9 @@
+function showTextArea(value) {
+    const textarea = document.getElementById("text2");
+    if (value == "1") {
+        textarea.style.display = "block";
+    }
+    else {
+        textarea.style.display = "none";
+    }
+}
